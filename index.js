0672b28d61936cf9a3533ad56ca4ef081865f0b6
sandip mondal <smondal2@wsgc.com>
2019-09-08 11:18:24 +0530

    Added spec and handing function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = obj => {
-	return obj;
+module.exports = (...obj) => {
+	let max;
+	if (Array.isArray(...obj)) {
+		max = Math.max.apply(null, ...obj);
+	}	else {
+		max = Math.max(...obj);
+	}
+
+	return max;
 };
--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 import {serial as test} from 'ava';
 import findMax from '.';
 
-test('foo', t => {
-	t.is(findMax('sandip'), 'sandip');
+test('arguments', t => {
+	t.is(findMax(1, 2, 3, 4), 4);
+});
+
+test('array', t => {
+	t.is(findMax([1, 2, 3, 4]), 4);
 });
