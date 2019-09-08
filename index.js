71d9016025303d59c24c23d643a921acc7e5fef9
sandip mondal <smondal2@wsgc.com>
2019-09-08 18:24:43 +0530

    Handle Object and no argument

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 module.exports = (...obj) => {
 	let max;
+	if(obj.flat().length==0)
+		return max
 	if (Array.isArray(...obj)) {
+		console.log('array')
 		max = Math.max.apply(null, ...obj);
+	}	else if (typeof obj === 'object' && Object.keys(...obj).length >= 1) {
+		console.log('obj')
+		max = Object.keys(...obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
 	}	else {
+		console.log('else')
 		max = Math.max(...obj);
 	}
 
